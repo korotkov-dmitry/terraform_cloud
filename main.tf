b77d7fa71236e6c9ca7c0027fcc432171f0e7a19
vagrant <vagrant@vagrant.vm>
2022-06-21 08:20:09 +0000

    HW

--- main.tf
+++ main.tf
@@ -20,9 +20,9 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "${var.YC_TOKEN}"
-  cloud_id  = "${var.YC_CLOUD_ID}"
-  folder_id = "${var.YC_FOLDER_ID}"
+  token     = var.YC_TOKEN
+  cloud_id  = var.YC_CLOUD_ID
+  folder_id = var.YC_FOLDER_ID
   zone      = "ru-central1-a"
 }
 
